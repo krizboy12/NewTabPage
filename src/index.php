0efa4e44bae7635746950f5d43f2bc6388e87fe9
krizboy12 <krizboy12@gmail.com>
2017-12-17 22:46:00 -0500

    Testing connection to sql server

--- src/index.php
+++ src/index.php
@@ -1,1 +1,20 @@
-<html><head><title>New Terb</title></head></html>
+<html>
+	<head>
+		<title>New Terb</title>
+	</head>
+	<body>
+		<?php
+			$server = "localhost";
+			$user = "newtab";
+			$pass = "bkxDqaNOHJHiy7Je";
+
+			$connection = new mysqli($server, $user, $pass);
+			
+			if ($connection->connect_error) {
+				die("Connection to MySQL server: " . $connection->connect_error);
+			}
+
+			echo "Connected Successfully";
+		?>
+	</body>
+</html>
