371abc87e741beff46ef9b5cc4e56a876870fe7e
krizboy12 <krizboy12@gmail.com>
2017-12-17 23:53:40 -0500

    added some jquery to show table contents from database

--- src/index.php
+++ src/index.php
@@ -1,8 +1,39 @@
 <html>
 	<head>
 		<title>New Terb</title>
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 	</head>
 	<body>
-		<?php include "getLinks.php"; ?>
+		<script>
+			function trimJSON(data) {
+				// extract the text to put in the log
+				var $regex = /\[?({(("[a-zA-Z_$][a-zA-Z_$0-9]*"):(".*"|null),?)+},?)+\]?/g;
+
+				var str = [
+					data.replace($regex, ""),
+					data.match($regex)
+				];
+
+				return str;
+			}
+
+			$.ajax({
+				url: "getLinks.php",
+				type: "GET",
+				success: function(data, status) {
+					var data = trimJSON(data);
+					console.log("Status: " + status + "\nData: " + data[0]);
+					var json = $.parseJSON(data[1]);
+
+					for (var index in json) {
+						$("body").append(json[index][label] + ": " + json[index][link]);
+					}
+				},
+				error: function(data, status) {
+					console.log("Status: " + status + "\nData: " + data);
+				}
+			})
+
+		</script>
 	</body>
 </html>
