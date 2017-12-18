dd530ce0127c74d49ba6df8680e257e423624793
krizboy12 <krizboy12@gmail.com>
2017-12-17 23:04:44 -0500

    Object oriented database connection

---
+++ src/Database.php
@@ -0,0 +1,31 @@
+<?php
+	class Database {
+		private $server = "localhost";
+		private $user = "newtab";
+		private $pass = "bkxDqaNOHJHiy7Je";
+		private $database = "newtab";
+
+		private $connection;
+
+		function __construct() {}
+
+		public function connect() {
+			if (!$connection) {
+				$connection = new mysqli($server, $user, $pass, $database);
+		
+				if ($connection->connect_error) {
+					die("Could not connect to MySQL server: " . $connection->connect_error . "\n");
+				}
+
+				echo "Connected successfully\n";
+			} else {
+				echo "Already connected\n";
+			}
+		}
+
+		public function diconnect() {
+			$connection->close();
+			echo "Connection closed\n";
+		}
+	}
+?>
--- src/index.php
+++ src/index.php
@@ -3,18 +3,6 @@
 		<title>New Terb</title>
 	</head>
 	<body>
-		<?php
-			$server = "localhost";
-			$user = "newtab";
-			$pass = "bkxDqaNOHJHiy7Je";
-
-			$connection = new mysqli($server, $user, $pass);
-			
-			if ($connection->connect_error) {
-				die("Connection to MySQL server: " . $connection->connect_error);
-			}
-
-			echo "Connected Successfully";
-		?>
+		<?php include "getLinks.php"; ?>
 	</body>
 </html>
